2ac0513c010af26feeaffe80159d432f418cd303
Jon Davis <jon@jondavis.net>
2012-09-19 10:08:58 -0700

    add using.js project v1.3.1

---
+++ dep2.js
@@ -0,0 +1,1 @@
+window.dep2 = 'loaded';
---
+++ helloworld.js
@@ -0,0 +1,6 @@
+window.hello_isLoaded = function(){
+	window.hello_isLoaded = function() {
+		return "I'm already loaded.";
+	}
+	return "Hello world! I'm loaded!!";
+}
